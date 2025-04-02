a030cb1776282f73ce56272665c3dac8f2f12dbb
Sanusi1502 <sanusiii1512@gmail.com>
2025-04-02 15:49:49 +0700

    create func to add two number

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function addNumber($numberOne, $numberTwo)
+    {
+        return $numberOne + $numberTwo;
+    }
 }
